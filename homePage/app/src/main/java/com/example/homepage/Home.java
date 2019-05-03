5f4ba14c74bd1cf884d0b351cc17ad909e22aab0
Anvesha Barnwal <anvesha@nv-she-a2.local>
2019-05-03 12:01:43 +0530

    new commit

--- homePage/app/src/main/java/com/example/homepage/Home.java
+++ homePage/app/src/main/java/com/example/homepage/Home.java
@@ -1,7 +1,10 @@
 package com.example.homepage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onButtonClick(View v){
+        Intent myIntent = new Intent(getBaseContext(), RequestActivity.class);
+        startActivity(myIntent);
     }
 }
